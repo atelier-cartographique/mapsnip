28effb8991052106a6592760ae7167f504ae9238
Pierre Marchand <pierremarc07@gmail.com>
2015-11-13 08:54:34 +0100

    something works!

--- static/svg.js
+++ static/svg.js
@@ -20,17 +20,46 @@ function getSVG () {
         while (container.firstChild) {
             container.removeChild(container.firstChild);
         }
-        var paper = Raphael(container, rect.width, rect.height);
-        for(var i = 0; i < results.length; i++) {
-            var p = paper.path(results[i].svg);
+        var paper = Raphael(container, rect.width, rect.height),
+            elems = paper.set(),
+            curX = 0,
+            curY = 0,
+            lastHeight = 0,
+            lineN = 4,
+            rLen = Math.min(results.length, 200),
+            cellN = Math.ceil(rLen / lineN),
+            lineHeight = rect.height / lineN,
+            cellWidth = rect.width / cellN;
+        for (var l = 0; l < lineN; l++) {
+            curY = l * lineHeight;
+            for (var c = 0; c < cellN; c++) {
+                curX = c * cellWidth;
+                var i = (l * cellN) + c;
+                if (i > (results.length - 1)) {
+                    break;
+                }
+                var ps = results[i].svg;
+                var bbox = Raphael.pathBBox(ps);
+                var m = Raphael.matrix(1,0,0,1,0,0);
+                var s = cellWidth / bbox.width
+                m.translate((-bbox.x) * s, (-bbox.y) * s);
+                m.scale(s);
+                m.translate(curX / s, curY / s);
+                var tps = Raphael.mapPath(ps, m);
+                // console.log(Raphael.pathBBox(tps));
+                elems.push( paper.path(tps) );
+            }
         }
+        elems.attr({
+            fill: 'black'
+        });
     });
 }
 
 
 $(document).ready(function(){
      map = L.map('map-container', {
-        center: [51.505, -0.09],
+        center: [50.850431249042046, 4.048461914062501],
         zoom: 13
     });
     L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png?', {
